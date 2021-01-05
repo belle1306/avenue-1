c9d34e9e2d9f9e2a559ca505425434a6dc8d19bf
LillianToh <lilliantoh1111@gmail.com>
2021-01-05 14:14:24 +0800

    showed modal has input fields

--- client/src/component/List/List.js
+++ client/src/component/List/List.js
@@ -3,27 +3,33 @@ import React from "react";
 const List = (props) => {
     const propertySummary = props.properties.map(p => {
         return (
-            <ul key={p.id}>
-                <li> Address: {p.property_address} </li>
-                <li> Postcode: {p.property_postcode} </li>
-                <li> Bedroom: {p.property_bedroom} </li>
-                <li> Bathroom: {p.property_bathroom} </li>
-                <li> Carpark: {p.property_carpark} </li>
-                <li> Furnishing: {p.property_furnish} </li>
-                <li> Photo:</li>
-            </ul>
+            <div key={p.id}>
+                Address
+                <input type="text" defaultValue={p.property_address}/>
+                Postcode
+                <input type="text" defaultValue={p.property_postcode}/>
+                Bedroom
+                <input type="text" defaultValue={p.property_bedroom}/>
+                Bathroom
+                <input type="text" defaultValue={p.property_bathroom}/>
+                Carpark
+                <input type="text" defaultValue={p.property_carpark}/>
+                Furnish
+                <input type="text" defaultValue={p.property_furnish}/>
+            </div>
         );
     });
     
     return (
         <div>
-             <h5>Editing...</h5>
-             <div>
-                 {propertySummary}
-             </div>
-             <button>Save</button>
+            <h5>Editing...</h5>
+            
+            {propertySummary}
+            
+            <button>Save</button>
         </div>
     );
 };
+//MOVE BUTTON TO APP; WHEN CLICK SAVE, SEND PUT REQ to API
 
 export default List;
