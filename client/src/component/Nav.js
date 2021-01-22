b6d886218b3139ffa8d733c99aefe1c44e30afad
LillianToh <56778700+LillianToh@users.noreply.github.com>
2021-01-22 16:44:59 +0800

    router for home, property manager done

--- client/src/component/Nav.js
+++ client/src/component/Nav.js
@@ -1,12 +1,14 @@
 import React from "react";
 // import "./App.css";
-
+import {Link} from'react-router-dom';
 function Nav() {
+    
     return (
         <nav className="nav">
             <ul className="nav-links">
-                <li>Home</li>
-                <li>Manager</li>    
+                <Link to="/manager">
+                    <li>Manager</li>
+                </Link>  
             </ul>
         </nav>
     );
