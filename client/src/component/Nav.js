2ffe647f84666f34d3503560051e39ffdf254792
LillianToh <56778700+LillianToh@users.noreply.github.com>
2021-01-22 17:37:49 +0800

    move toolbar to new nav bar

--- client/src/component/Nav.js
+++ client/src/component/Nav.js
@@ -1,16 +1,26 @@
 import React from "react";
 // import "./App.css";
 import {Link} from'react-router-dom';
-function Nav() {
-    
+import Logo from "./Logo/Logo";
+import Facebook from './Facebook';
+
+function Nav(props) {
     return (
+    <header>
         <nav className="nav">
             <ul className="nav-links">
+                <Logo />
                 <Link to="/manager">
                     <li>Manager</li>
-                </Link>  
+                    <h1 className="text-white m-4">Avenue</h1>
+                    <button className="btn btn-danger text-white m-4" onClick={props.newbtn}>New</button>
+                    <button className="btn btn-danger text-white m-4" onClick={props.calculatebtn}>Rent Calculator</button>
+                    <button className="btn btn-danger text-white" onClick={props.logoutbtn}>Log out</button>
+                </Link> 
+                <Facebook />
             </ul>
         </nav>
+    </header>
     );
 }
 
--- client/src/component/Navigation/Toolbar/Toolbar.js
+++ client/src/component/Navigation/Toolbar/Toolbar.js
@@ -1,10 +1,9 @@
 import React from "react";
-import classes from "./Toolbar.css";
 import Logo from "../../Logo/Logo";
 import Facebook from '../../Facebook';
 
 const toolbar = (props) => (
-    <header className={classes.Toolbar}>
+    <header>
         <nav className="navbar navbar-expand-lg navbar-danger bg-danger ml-auto">
             <Logo />
             <h1 className="text-white m-4">Avenue</h1>
