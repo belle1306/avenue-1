d4243bafd8d5a4f7f792c74299af276a916368f5
LillianToh <56778700+LillianToh@users.noreply.github.com>
2021-01-22 17:18:07 +0800

    remove toolbar

--- client/src/component/Nav.js
+++ client/src/component/Nav.js
@@ -1,8 +1,8 @@
 import React from "react";
 // import "./App.css";
 import {Link} from'react-router-dom';
+
 function Nav() {
-    
     return (
         <nav className="nav">
             <ul className="nav-links">
