60a5ecd5f501d4cebfd00f23827a689a692a5ea9
LillianToh <56778700+LillianToh@users.noreply.github.com>
2021-01-22 16:04:39 +0800

    setting up router

---
+++ client/src/component/Nav.js
@@ -0,0 +1,15 @@
+import React from "react";
+// import "./App.css";
+
+function Nav() {
+    return (
+        <nav className="nav">
+            <ul className="nav-links">
+                <li>Home</li>
+                <li>Manager</li>    
+            </ul>
+        </nav>
+    );
+}
+
+export default Nav;
---
+++ client/src/component/Owner.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import "./App.css";
+
+function Owner() {
+    return (
+        <div>
+            <h1>Owner Page</h1>
+        </div>
+    );
+}
+
+export default Owner;
