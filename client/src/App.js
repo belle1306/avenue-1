e2151ca8b194e584fad6dc720df0cc7cf04dacb0
jas4gan <jas4gan@gmail.com>
2021-02-13 22:57:01 +0800

    fb button, protected route owner, a href login link to home, db manager table

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import Nav from './component/Nav';
@@ -7,115 +6,108 @@ import Manager from './component/Manager';
 import Login from './component/Login';
 import Owner from './component/Owner';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import ProtectedRoute from "./hoc/ProtectedRoute";
 // import classes from "./component/Nav.module.css";
 // import ProtectedRoute from "./auth/protected-route";
 import bgVideo from "./assets/images/pexels_02.mp4";
 import Logo from "./component/Logo/Logo";
 import Footer from "./component/Footer";
 import FacebookLogin from 'react-facebook-login';
-import owner from "../src/assets/owner.json";
-require('dotenv').config();
-// const env = require("dotenv").config();
-// require("dotenv").config();
-// const mysql = require("mysql");
-console.log(process.env, "ENV<<")
 
 function App() {
   const [login, setLogin] = useState(false);
   const [data, setData] = useState(null);
-  const [picture, setPicture] = useState(null);
   const [isManager, setManager] = useState(false);
   const [isOwner, setOwner] = useState(false);
-  // const MANAGER_EMAIL = "lilliantoh111@hotmail.com";
-  // const OWNER_EMAIL = "lilliantoh111@hotmail.com";
-  const OWNER_EMAIL = "jaseslin@gmail.com";
-  const MANAGER_EMAIL = "jaseslin@gmail.com";
-  const OWNER_EMAIL_2 = "prakashnitza@gmail.com";
-  const ownerEmail = owner.owners.map(e => e.email);
   const [oId, setId] = useState(null);
   const [currentData, setCurrentData] = useState([]);
+  const MANAGER_EMAIL = "lilliantoh111@hotmail.com";
 
   useEffect(() => {
-
     console.log(isOwner, "isOwner at start");
     fetch("/propertymgmt/owners")  // should only happen for data.email
       .then(res => res.json())
       .then(data => {
         console.log(data);
+        data.email = "";
         setCurrentData(data)
         console.log(currentData, "<<< state after set state");
       })
-    // console.log(ownerEmail, "WE WANT TO COMPARE");
-    // 
     currentData.map(d => {
-      console.log(data.email , " from fb is compared with ", d.owner_email );
-      (d.owner_email === data.email) ? setId(d.id) : console.log("NOT FOUND");
+      console.log(data.email, " from fb is compared with ", d.owner_email);
+      if (d.owner_email === data.email) {
+        setId(d.id);
+      } else {
+        console.log("NOT FOUND");
+      }
     });
-
   }, [isOwner])
 
-  const responseFacebook = (response) => {
-    // console.log(response);
+  const logoutFacebook = (response) => {
     setData(response);
-    setPicture(response.picture.data.url);
     if (response.accessToken) {
-      setLogin(true);
-      (response.email === MANAGER_EMAIL) ?
-        // (response.email === process.env.MANAGER_EMAIL) ?
-        setManager(true) : setOwner(true);
-    } else {
       setLogin(false);
+      setManager(false);
+      setOwner(false);
+      setId(null);
     }
-    console.log(isManager, "Manager<<");
-    console.log(isOwner, "Owner<<");
-    console.log(process.env.REACT_APP_MANAGER_EMAIL, "EMAIL<<");
-  }
+  };
+
+  const responseFacebook = (response) => {
+    console.log(response, "r u here");
+    if (response.status !== 'unknown') {
+      setData(response);
+      if (response.accessToken) {
+        setLogin(true);
+        (response.email === MANAGER_EMAIL) ?
+          setManager(true) : setOwner(true);
+      } else {
+        setLogin(false);
+      }
+    };
+  };
+
   return (
-    <div>      
-      <Router>        
-        {/* (isLoggedIn) ? <NavLoggedIn /> : <Nav /> */}
-        <Nav oId={oId} />    
-        <div class="container">
-          <div style={{ width: '600px' }}>
-            <div>
-              {!login &&
-                <FacebookLogin
-                  appId="845362466033294"
-                  autoLoad={true}
-                  fields="name,email,picture"
-                  scope="public_profile,user_friends"
-                  callback={responseFacebook}
-                  icon="fa-facebook" />
-              }
-              {login &&
-                <img src={picture} roundedCircle />
-              }
-            </div>
-            {/* {login &&
-            <div>
-              <div>{data.name}</div>
-              <div>
-                {data.email}
-              </div>
-            </div>
-          } */}
-          </div>
+    <div>
+      <Router>
+        <Nav oId={oId} />
+        <div className="text-end m-3">
+          {!login &&
+            <FacebookLogin
+              appId="845362466033294"
+              autoLoad={false}
+              fields="name,email,picture"
+              scope="public_profile,user_friends"
+              callback={responseFacebook}
+              cssClass="kep-login-facebook kep-login-facebook-small"
+              textButton=" Login"
+              icon="fa-facebook" />
+          }
+          {login &&
+            <FacebookLogin
+              appId=""
+              autoLoad={false}
+              fields="name,email,picture"
+              scope="public_profile,user_friends"
+              callback={logoutFacebook}
+              cssClass="kep-login-facebook kep-login-facebook-small"
+              textButton=" Logout"
+              icon="fa-facebook" />
+          }
         </div>
         <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/manager" exact component={isManager ? Manager : Home} />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/manager" component={isManager ? Manager : Home} />
           <Route path="/login" component={Login} />
-          <Route path="/owner/:id" exact component={isOwner ? Owner : OwnerHome} />
+          <ProtectedRoute exact path="/owner/:id" component={isOwner ? Owner : OwnerHome} />
         </Switch>
       </Router>
-      
     </div>
   );
-}
+};
 
 const Home = () => (
   <div>
-    {/* <div className="video-overlay"> */}
     <video autoPlay loop muted id="background-video">
       <source src={bgVideo} type='video/mp4' />
     </video>
@@ -123,7 +115,6 @@ const Home = () => (
       <h1 className="display-1 text-center"><Logo />Avenue</h1>
       <h3 className="display-6 text-center">A Vision for Property Management</h3>
     </div>
-    {/* </div> */}
   </div>
 );
 
@@ -131,12 +122,12 @@ const OwnerHome = () => (
   <div>
     <div className="text-position" id="ownerHomeimg">
       <h2 className="display-2 text-end">You're not on Avenue yet, &nbsp;</h2>
-      <button id="btn-redirect" className="btn btn-danger btn-lg text-light mt-3" >but you're one step away.</button>
+      <div className="text-end m-3">
+        <a className="btn btn-danger btn-lg text-light" href="/">but you're one step away.</a>
+      </div>
     </div>
     <Footer />
   </div>
 );
 
-
 export default App;
-
--- routes/api.js
+++ routes/api.js
@@ -13,7 +13,7 @@ router.get("/", (req, res) => {
 // GET all tables - properties, owners and leases
 router.get("/propertymgmt/:table", (req, res) => {
   const table = req.params.table;
-  console.log('table>>> ',table);
+  console.log('table>>> ', table);
   db(`SELECT * FROM ${table};`)
     .then(results => {
       res.send(results.data);
@@ -46,10 +46,25 @@ router.get("/propertymgmt/owners/:id", (req, res) => {
       res.send(results.data);
     })
     .catch(err => res.status(500).send(err));
-}); 
+});
 
 router.post("/propertymgmt/managers", (req, res) => {
-  
+
+})
+
+//Post owner email and Get specific owner, tenant, property data
+router.get("/propertymgmt/properties/owners/getId", async (req, res) => {
+  const newOwnerEmail = req.body.owner_email;
+  console.log("---REQ.BODY.OWNER_EMAIL----", newOwnerEmail);
+  db(`SELECT id FROM owners WHERE owner_email='${newOwnerEmail}';`)
+    .then(results => {
+      res.send(results.data);
+    })
+    .catch(err => res.status(500).send(err));
+});
+
+router.post("/propertymgmt/properties/owners/getId", (req, res) => {
+
 })
 // POST new property after user has a choice to select which owner
 // populate owner table with fake data
@@ -59,7 +74,7 @@ router.post("/propertymgmt/properties", (req, res) => {
   // console.log("----------3------", JSON.stringify(req.body))
   // const thbody = JSON.stringify(req.body);
   // console.log(thbody.property_address);
-  console.log("---THIS IS BEING PASSED----", req.body.property_address,req.body.owner_id)
+  console.log("---THIS IS BEING PASSED----", req.body.property_address, req.body.owner_id)
   const newAddress = req.body.property_address;
   const newOwnerId = req.body.owner_id;
   const newPostcode = req.body.property_postcode;
@@ -113,11 +128,11 @@ router.put("/propertymgmt/properties/:property_id", (req, res) => {
 });
 
 //Create signature request
-router.post("/owner/callback", (req, res,json) => {
+router.post("/owner/callback", (req, res, json) => {
   const data = req.body.json;
   console.log("Input payload of the event-----", data);
-  res.status(200).send('Hello API Event Received'); 
-  
+  res.status(200).send('Hello API Event Received');
+
   process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
   const opts = {
     test_mode: 1,
@@ -127,7 +142,7 @@ router.post("/owner/callback", (req, res,json) => {
     message: 'I am glad we agreed',
     signers: [
       {
-        email_address: 'lilliantoh1111@gmail.com',
+        email_address: 'jaseslin@gmail.com',
         name: 'Tenant',
       },
       {
@@ -139,25 +154,26 @@ router.post("/owner/callback", (req, res,json) => {
   };
   hellosign.signatureRequest.createEmbedded(opts).then((res) => {
     // handle response
-    console.log(res.signature_request.client_id,"res")
+    console.log(res.signature_request.client_id, "res")
     const signature = res.signature_request.signatures[0];
     const signature1 = res.signature_request.signatures[1];
-    
+
     // console.log("signature", res.signature_request.signatures);
     const signatureId = signature.signature_id;
     const signatureId1 = signature1.signature_id;
     console.log("signature Id1", signatureId1);
     console.log("signature Id", signatureId);
     // return hellosign.embedded.getSignUrl(signatureId);
-    return hellosign.embedded.getSignUrl(signatureId),hellosign.embedded.getSignUrl(signatureId1);
+    return hellosign.embedded.getSignUrl(signatureId), hellosign.embedded.getSignUrl(signatureId1);
   }).then(result => {
     // console.log('The sign url: ' + result.embedded);
     console.log('The sign url: ' + JSON.stringify(result.embedded.sign_url));
+    console.log('The sign url: ' + JSON.stringify(result));
     // console.log('The sign url: ' + result.embedded.sign_url);
     // console.log('embedded ', result.embedded);
     res.send(JSON.stringify(result.embedded.sign_url));
   })
-  .catch(err => res.status(500).send(err));  
+    .catch(err => res.status(500).send(err));
 })
 
 module.exports = router;
